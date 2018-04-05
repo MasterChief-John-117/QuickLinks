d135682c1399d85489f3922a5c01fd3c53dce33b
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-04-05 07:45:36 -0700

    Removed unused routes

--- src/QuickLinks/Controllers/BasicController.cs
+++ src/QuickLinks/Controllers/BasicController.cs
@@ -1,40 +1,13 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace QuickLinks.Controllers
 {
     [Route("/")]
     public class ValuesController : Controller
     {
-        // GET /
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return null;
-        }
-
-        // GET /5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return null;
-        }
-
-        // POST /
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT /5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE /5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // GET /anything
+        [HttpGet("{*url}")]
+        public IActionResult Get(string url)
         {
         }
     }
