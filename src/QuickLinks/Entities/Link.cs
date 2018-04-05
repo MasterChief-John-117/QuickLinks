c5d2562145792f54868efb25efa7bedc3d8f1e56
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-04-05 11:10:41 -0700

    Ensure uniqueness in link

--- src/QuickLinks/Entities/Link.cs
+++ src/QuickLinks/Entities/Link.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -13,39 +14,60 @@ namespace QuickLinks.Entities
             public string Ip;
         }
         public string OriginalUrl { get; private set; }
+        [BsonId]
         public string ShortUrl { get; private set; }
         public string AnalyticsTag { get; private set; }
         public List<Visitor> AllVisitors { get; private set; }
         public HashSet<Visitor> UniqueVisitors { get; private set; }
 
+        public Link()
+        {
+
+        }
         public Link(string url)
         {
             OriginalUrl = url;
-            AnalyticsTag = GetNewAnalyticsTag();
             ShortUrl = GetNewShortLink();
+            AnalyticsTag = GetNewAnalyticsTag();
             AllVisitors = new List<Visitor>();
             UniqueVisitors = new HashSet<Visitor>();
         }
 
         private string GetNewShortLink()
         {
-            var words = Program.database.GetCollection<Program.Word>("words").FindAll().Select(w => w.Value);
-            string val = "";
-            val += words.ElementAt((new Random()).Next(0, words.Count()));
-            val += words.ElementAt((new Random()).Next(0, words.Count()));
-            val += words.ElementAt((new Random()).Next(0, words.Count()));
-            return val;
+            var linkDb = Program.database.GetCollection<Link>("links");
+            while (true)
+            {
+                Console.WriteLine("Generating new Shortlink");
+                var words = Program.database.GetCollection<Program.Word>("words").FindAll().Select(w => w.Value);
+                string val = "";
+                val += words.ElementAt((new Random()).Next(0, words.Count()));
+                val += words.ElementAt((new Random()).Next(0, words.Count()));
+                val += words.ElementAt((new Random()).Next(0, words.Count()));
+                if (!linkDb.Exists(l => l.ShortUrl == val))
+                {
+                    return val;
+                }
+            }
         }
 
         private string GetNewAnalyticsTag()
         {
-            char[] chars = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-            string tag = "";
-            for(int i = 0; i < 16; i++)
+            var linkdb = Program.database.GetCollection<Link>("links");
+            while (true)
             {
-                tag += chars[NextInt()];
+                Console.WriteLine("Generating new Analytics tag");
+                char[] chars = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+                string tag = "";
+                for (int i = 0; i < 16; i++)
+                {
+                    tag += chars[NextInt()];
+                }
+                if(!linkdb.Exists(l => l.AnalyticsTag == tag))
+                {
+                    return tag;
+                }
             }
-            return tag;
         }
 
         private int NextInt()
