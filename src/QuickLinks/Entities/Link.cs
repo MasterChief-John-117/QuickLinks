2aad679b8e6ff747af02056ecb2a941fefb8b0d6
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-04-04 23:59:37 -0700

    Added Link object

---
+++ src/QuickLinks/Entities/Link.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace QuickLinks.Entities
+{
+    public class Link
+    {
+        public class Visitor
+        {
+            public string UserAgent;
+            public string Ip;
+        }
+        public string OriginalUrl { get; private set; }
+        public string ShortUrl { get; private set; }
+        public string AnalyticsTag { get; private set; }
+        public List<Visitor> AllVisitors { get; private set; }
+        public HashSet<Visitor> UniqueVisitors { get; private set; }
+
+        public Link(string url)
+        {
+            OriginalUrl = url;
+            AnalyticsTag = GetNewAnalyticsTag();
+            ShortUrl = GetShortLink();
+            AllVisitors = new List<Visitor>();
+            UniqueVisitors = new HashSet<Visitor>();
+        }
+
+        private string GetShortLink()
+        {
+            var words = Program.database.GetCollection<Program.Word>("words").FindAll().Select(w => w.Value);
+            string val = "";
+            val += words.ElementAt((new Random()).Next(0, words.Count()));
+            val += words.ElementAt((new Random()).Next(0, words.Count()));
+            val += words.ElementAt((new Random()).Next(0, words.Count()));
+            return val;
+        }
+
+        private string GetNewAnalyticsTag()
+        {
+            char[] chars = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            string tag = "";
+            for(int i = 0; i < 16; i++)
+            {
+                tag += chars[NextInt()];
+            }
+            return tag;
+        }
+
+        private int NextInt()
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buffer = new byte[4];
+
+            rng.GetBytes(buffer);
+            return Math.Abs( BitConverter.ToInt32(buffer, 0) % 26);
+        }
+    }
+}
