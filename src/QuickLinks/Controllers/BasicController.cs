f51644c0fff04e9f240b8a9a73c9f2f7a5bba8d1
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-04-05 11:16:07 -0700

    get handles creating and retrieving link

--- src/QuickLinks/Controllers/BasicController.cs
+++ src/QuickLinks/Controllers/BasicController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace QuickLinks.Controllers
 {
@@ -9,6 +10,24 @@ namespace QuickLinks.Controllers
         [HttpGet("{*url}")]
         public IActionResult Get(string url)
         {
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                var link = new Entities.Link(url);
+                Program.database.GetCollection<Entities.Link>("links").Insert(link);
+                return new JsonResult(link);
+            }
+            else
+            {
+                var link = Program.database.GetCollection<Entities.Link>("links").FindById(url);
+                if (link != null)
+                {
+                    return RedirectPermanent(link.OriginalUrl);
+                }
+                else
+                {
+                    return new JsonResult("NOT FOUND");
+                }
+            }
         }
     }
 }
