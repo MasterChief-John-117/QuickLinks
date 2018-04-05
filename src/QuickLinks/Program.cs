af1c174c97eca229099323b415967cd8e9617f92
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-04-04 21:54:16 -0700

    Initialize the word list in the database

--- src/QuickLinks/Program.cs
+++ src/QuickLinks/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using LiteDB;
+using System.IO;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace QuickLinks
 {
@@ -9,7 +13,47 @@ namespace QuickLinks
         public static LiteDatabase database = new LiteDatabase(@"filename=lite.db; mode=exclusive; password=YOURPASSWORDHERE");
         public static void Main(string[] args)
         {
+            InitDB();
             WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build().Run();
+        }
+
+        private static void InitDB()
+        {
+            if (!database.CollectionExists("words"))
+            {
+                var wordDb = database.GetCollection<Word>("words");
+                if (!File.Exists("words.txt"))
+                {
+                    Console.WriteLine("Word list does not exist. Please ensure it is named 'words.txt', and is placed in the root directory. If not please check the original repository to download the list");
+                    Console.WriteLine("Press the any key to exit");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                string line;
+                var file = new StreamReader("words.txt");
+                while (!string.IsNullOrEmpty((line = file.ReadLine())))
+                {
+                    try
+                    {
+                        wordDb.Insert(new Word { Value = line });
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        Console.ReadKey();
+                    }
+                }
+                file.Close();
+                stopwatch.Stop();
+                Console.WriteLine($"Created word list in {stopwatch.ElapsedMilliseconds} ms");
+            }
+        }
+        public class Word
+        {
+            [BsonId]
+            public string Value { get; set; }
         }
     }
 }
