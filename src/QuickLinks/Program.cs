15f5429cb677cd5a1e5d83677df2ad0a4a762b72
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-04-04 19:46:36 -0700

    Connection to database

--- src/QuickLinks/Program.cs
+++ src/QuickLinks/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using LiteDB;
 
 namespace QuickLinks
 {
     public class Program
     {
+        public static LiteDatabase database = new LiteDatabase(@"filename=lite.db; mode=exclusive; password=YOURPASSWORDHERE");
         public static void Main(string[] args)
         {
             WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build().Run();
