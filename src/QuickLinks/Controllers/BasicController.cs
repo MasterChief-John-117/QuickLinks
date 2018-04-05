cc12a244f34d8eb67f930be9ad79daba16434a95
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-04-04 18:17:25 -0700

    Created basic dotnet core API

---
+++ src/QuickLinks/Controllers/BasicController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickLinks.Controllers
+{
+    [Route("/")]
+    public class ValuesController : Controller
+    {
+        // GET /
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return null;
+        }
+
+        // GET /5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return null;
+        }
+
+        // POST /
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT /5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE /5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
